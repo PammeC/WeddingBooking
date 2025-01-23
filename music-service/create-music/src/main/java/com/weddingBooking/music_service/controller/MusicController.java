abdc5eecb6d2601b98473ba3d93870680582dfb7
PammeC <pamelajnth@gmail.com>
2025-01-23 16:25:28 -0500

    feat: :sparkles: Add create functionality to the music module

--- music-service/create-music/src/main/java/com/weddingBooking/music_service/controller/MusicController.java
+++ music-service/create-music/src/main/java/com/weddingBooking/music_service/controller/MusicController.java
@@ -1,4 +1,22 @@
 package com.weddingBooking.music_service.controller;
 
+import com.weddingBooking.music_service.model.Music;
+import com.weddingBooking.music_service.service.MusicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("musicC")
 public class MusicController {
+
+    @Autowired
+    private MusicService musicService;
+    @PostMapping("/create")
+    private String createMusic(@RequestBody Music music){
+        musicService.saveMusic(music);
+        return "Music service create successfully";
+    }
 }
