c07046230ffebf99aab61533281a3fcfb77ddafa
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2025-01-23 23:25:36 -0500

    feat: :tada: Province and location microservices - First version

---
+++ Provinces-Locations/microservice-locations/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
+const cors = require('cors'); // Importar cors
+
+async function startServer() {
+  const app = express();
+
+  // Habilitar CORS
+  app.use(cors());
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  const PORT = 4002;
+  app.listen(PORT, () => {
+    console.log(`Locations service running on http://localhost:${PORT}/graphql`);
+  });
+}
+
+startServer();
---
+++ Provinces-Locations/microservice-provinces/dbConfig.js
@@ -0,0 +1,14 @@
+// microservice-provinces/dbConfig.js o microservice-locations/dbConfig.js
+require('dotenv').config(); // Cargar variables de entorno desde .env
+const mysql = require('mysql2/promise');
+
+// Configuración de la conexión a la base de datos
+const db = mysql.createPool({
+  host: process.env.DB_HOST,       // Host de la base de datos
+  user: process.env.DB_USER,       // Usuario de la base de datos
+  password: process.env.DB_PASSWORD, // Contraseña de la base de datos
+  database: process.env.DB_NAME,   // Nombre de la base de datos
+  port: process.env.DB_PORT || 3306 // Puerto de la base de datos (por defecto 3306)
+});
+
+module.exports = db;
