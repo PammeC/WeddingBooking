dc5a1cc527edb7380b7e5446ce4f1605913522a8
PammeC <pamelajnth@gmail.com>
2025-01-30 09:17:11 -0500

    feat: :sparkles: Add create functionality to the catering module

---
+++ catering-service/read-catering/src/main/java/com/weddingBooking/catering_service/controller/CateringController.java
@@ -0,0 +1,37 @@
+package com.weddingBooking.catering_service.controller;
+
+import com.weddingBooking.catering_service.model.Catering;
+import com.weddingBooking.catering_service.service.CateringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class CateringController {
+
+    @Autowired
+    private CateringService cateringService;
+
+    @QueryMapping
+    public List<Catering> getAllCatering() {
+        return cateringService.getAllCatering();
+    }
+
+    @QueryMapping
+//    public Optional<Catering> getCateringById(@Argument Long id_catering) {
+//        return cateringService.getCateringById(id_catering);
+//    }
+    public Catering getCateringById(@Argument Long id_catering){
+        return cateringService.getCateringById(id_catering);
+    }
+
+    @QueryMapping
+    public List<Catering> searchCateringByName(@Argument String name) {
+        return cateringService.searchCateringByName(name);
+    }
+}
