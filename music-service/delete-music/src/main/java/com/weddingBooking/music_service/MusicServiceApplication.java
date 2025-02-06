2659986909f07671fac3cbffae3bb56b7dc995f8
PammeC <pamelajnth@gmail.com>
2025-02-06 17:18:13 -0500

    feat: :sparkles: Add pipeline to delete-music service

--- music-service/delete-music/src/main/java/com/weddingBooking/music_service/MusicServiceApplication.java
+++ music-service/delete-music/src/main/java/com/weddingBooking/music_service/MusicServiceApplication.java
@@ -1,6 +1,5 @@
 package com.weddingBooking.music_service;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,18 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusicServiceApplication {
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.configure().load();
-
-		System.setProperty("server.port", dotenv.get("SERVER_PORT"));
-		System.setProperty("spring.application.name", dotenv.get("SPRING_APPLICATION_NAME"));
-		System.setProperty("eureka.client.service-url.defaultZone", dotenv.get("EUREKA_CLIENT_SERVICE_URL"));
-		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("spring.jpa.database-platform", dotenv.get("SPRING_JPA_DATABASE_PLATFORM"));
-		System.setProperty("spring.jpa.hibernate.ddl-auto", dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
-
 		SpringApplication.run(MusicServiceApplication.class, args);
 	}
 
