cf163fc7b4db01b9e83c0c3c77d519e3cd53ca95
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2025-02-07 22:35:37 -0500

    feat: :wrench: Variable update for connection with database

--- Provinces-Locations/microservice-provinces-get/src/database/dbConfig.js
+++ Provinces-Locations/microservice-provinces-get/src/database/dbConfig.js
@@ -7,7 +7,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,       // Host de la base de datos
   user: process.env.DB_USER,       // Usuario de la base de datos
   password: process.env.DB_PASSWORD, // Contraseña de la base de datos
-  database: process.env.DB_NAME,   // Nombre de la base de datos
+  database: process.env.DB_NAME_L_P,   // Nombre de la base de datos
   port: process.env.DB_PORT || 3306 // Puerto de la base de datos (por defecto 3306)
 });
 
