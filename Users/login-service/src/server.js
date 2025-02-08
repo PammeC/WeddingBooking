52adbc6485457d4458e9e2b94b89d42bb40b3d8c
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2025-02-07 22:22:30 -0500

    feat: :tada: Add pipeline login-service

--- Users/login-service/src/server.js
+++ Users/login-service/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 const PORT = process.env.PORT || 3004;
 
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
   
