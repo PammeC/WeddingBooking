e036acc7c0dff89c26fcf78b14c27c3630ea6710
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2025-02-07 22:17:06 -0500

    feat: :tada: Add pipeline get-user-service

--- Users/get-user-service/src/routes/userRoutes.js
+++ Users/get-user-service/src/routes/userRoutes.js
@@ -3,6 +3,7 @@ const { getAllUsers, getUserById } = require('../controllers/userController');
 
 const router = express.Router();
 
+
 // Rutas para obtener usuarios
 router.get('/users', getAllUsers); // Obtener todos los usuarios
 router.get('/users/:id', getUserById); // Obtener un usuario por ID
