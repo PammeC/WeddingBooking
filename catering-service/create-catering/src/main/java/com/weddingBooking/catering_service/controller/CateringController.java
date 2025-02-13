00b9222a05dd4de843cd0afb35f053f94bc35a61
PammeC <pamelajnth@gmail.com>
2025-02-13 09:08:31 -0500

    feat: :sparkles: Add Swagger to create-catering service

--- catering-service/create-catering/src/main/java/com/weddingBooking/catering_service/controller/CateringController.java
+++ catering-service/create-catering/src/main/java/com/weddingBooking/catering_service/controller/CateringController.java
@@ -2,6 +2,7 @@ package com.weddingBooking.catering_service.controller;
 
 import com.weddingBooking.catering_service.model.Catering;
 import com.weddingBooking.catering_service.service.CateringService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cateringC")
+@Tag(name = "Catering API", description = "Operations to create catering services")
+
 public class CateringController {
 
     @Autowired
