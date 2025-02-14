ad9be0516a7aa4b81f9fa1c84839e18203efdf00
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2025-02-13 21:08:47 -0500

    feat: :sparkles: add cors in login users

--- Users/login-service/src/app.js
+++ Users/login-service/src/app.js
@@ -1,25 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Agregar esta línea
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-// CORS Middleware
-app.use(cors());
-
-// Configuración más específica de CORS
+// Configuración de CORS
 const corsOptions = {
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-  };
+};
+
+// CORS Middleware
+app.use(cors(corsOptions)); 
 
 // Middleware
 app.use(bodyParser.json());
 
-// Rutes
+// Rutas
 app.use('/api', authRoutes);
 
 module.exports = app;
