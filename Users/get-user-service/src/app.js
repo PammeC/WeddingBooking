fbf1cdadb94a0caf144c10bdb0f7c46831a14b03
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2025-02-13 23:37:02 -0500

    feat: :sparkles: uodate get users

--- Users/get-user-service/src/app.js
+++ Users/get-user-service/src/app.js
@@ -1,25 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Asegúrate de importar cors
-const userRoutes = require('./routes/userRoutes');
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const userRoutes = require("./routes/userRoutes")
 
-const app = express();
+const app = express()
 
-// Configuración de CORS
-const corsOptions = {
-    origin: ['http://localhost:3000'], // Permitir solicitudes desde este origen
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
-    credentials: true, // Permitir envío de cookies o credenciales
-};
-
-// Aplicar CORS con las opciones configuradas
-app.use(cors(corsOptions));
+// Configuración de CORS más permisiva para desarrollo
+app.use(cors())
 
 // Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // Rutas
-app.use('/api', userRoutes);
+app.use("/api", userRoutes)
 
-module.exports = app;
+module.exports = app
+
