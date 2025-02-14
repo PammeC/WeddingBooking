33982a29ca7f6e688a4040dca7f8e5a2e46d51f4
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2025-02-13 21:29:31 -0500

    feat: :sparkles: add cors in update

--- Users/update-user-service/src/app.js
+++ Users/update-user-service/src/app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Agregar esta línea
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// CORS Middleware
-app.use(cors());
-// Configuración más específica de CORS
+// Configuración de CORS
 const corsOptions = {
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-  };
+};
+
+// CORS Middleware
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(bodyParser.json());
 
-// Rutes
-
+// Rutas
 app.use('/api', userRoutes);
 
 module.exports = app;
